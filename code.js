015d6fe01c8cc4e109c3c2a43471b0eee7b3e99a
María <mairinikki@hotmail.com>
2024-06-18 20:27:23 -0300

    ...

--- code.js
+++ code.js
@@ -18,3 +18,56 @@ function limpiar()
     document.getElementById("usuario").value = "";
     document.getElementById("clave").value = "";
 }
+
+function inscribir() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("dni").value = "";
+    document.getElementById("edad").value = "";
+    document.getElementsByName("sexo").value = "";
+    document.getElementById("curso").value = "";
+  
+    document.getElementById("resultado").textContent = "¡Inscripción Exitosa!";
+  }
+  
+  function precio() {
+    var v1 = document.getElementById("marca").value;
+    var v2 = document.getElementById("modelo").value;
+  
+    if (v1 == 1 && v2 == 1) {
+      document.getElementById("resultado2").textContent =
+        "EL PRECIO ES 12.000.000 DE PESOS";
+    }
+  
+    if (v1 == 1 && v2 == 2) {
+      document.getElementById("resultado2").textContent =
+        "EL PRECIO ES 10.000.000 DE PESOS";
+    }
+  
+    if (v1 == 1 && v2 == 3) {
+      document.getElementById("resultado2").textContent =
+        "EL PRECIO ES 11.000.000 DE PESOS";
+    }
+  }
+  
+  function calcular() {
+    var valor1 = parseFloat(document.getElementById("n1").value);
+    var valor2 = parseFloat(document.getElementById("n2").value);
+    var op = document.getElementById("opera").value;
+    var resultado;
+  
+    if (op == 1) {
+      resultado = valor1 + valor2;
+    }
+  
+    if (op == 2) {
+      resultado = valor1 - valor2;
+    }
+  
+    if (op == 3) {
+      resultado = valor1 * valor2;
+    }
+  
+    document.getElementById("resultado2").textContent =
+      "EL RESULTADO ES:" + resultado;
+  }
